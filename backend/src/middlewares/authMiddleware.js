d8afc9a2ef91ff728e262cfc7f8d42043273d47b
sumit <sumitshukla4020@gmail.com>
2025-07-27 01:20:26 +0530

    auth error remove

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import redisClient from "../services/redis.service.js";
+import redisClient from "../config/redis.js";
 
 
 export const authUser = async (req, res, next) => {
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,18 +1,42 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-}, { timestamps: true });
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: [ 6, 'Email must be at least 6 characters long' ],
+        maxLength: [ 50, 'Email must not be longer than 50 characters' ]
+    },
 
-userSchema.pre("save", async function () {
-  if (!this.isModified("password")) return;
-  this.password = await bcrypt.hash(this.password, 12);
-});
+    password: {
+        type: String,
+        select: false,
+    }
+})
 
-userSchema.methods.comparePassword = function (password) {
-  return bcrypt.compare(password, this.password);
-};
 
-export default mongoose.model("User", userSchema);
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+userSchema.methods.isValidPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateJWT = function () {
+    return jwt.sign(
+        { email: this.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '24h' }
+    );
+}
+
+
+const User = mongoose.model('user', userSchema);
+
+export default User;
